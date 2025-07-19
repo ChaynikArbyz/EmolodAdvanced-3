6a80a3b1a9f02b54daa32e90f8280af03bade8d4
ChaynikArbyz <romikbro.133@gmail.com>
2025-07-19 23:44:56 +0300

    Completed

---
+++ ContactBook/ContactBook/Contacts.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesAndPasswordGenerator
+{
+    internal static class Contacts
+    {
+        public static void AddContact(Dictionary<string,string> contacts)
+        {
+            Console.Clear();
+            ColorText.WriteColorLine("Введіть ім'я контакту:", ConsoleColor.Cyan);
+            string name = Console.ReadLine();
+            ColorText.WriteColorLine("Введіть номер телефону:", ConsoleColor.Magenta);
+            string phoneNumber = Console.ReadLine();
+            if (contacts.ContainsKey(name))
+            {
+                ColorText.WriteColorLine("Контакт з таким ім'ям вже існує!", ConsoleColor.Red);
+                Console.ReadKey(true);
+            }
+            else
+            {
+                contacts.Add(name, phoneNumber);
+                ColorText.WriteColorLine("Контакт успішно додано!", ConsoleColor.Green);
+                Console.ReadKey(true);
+            }
+        }
+
+        public static void ShowContacts(Dictionary<string, string> contacts) 
+        {
+            Console.Clear();
+            if (contacts.Count < 1)
+            {
+                ColorText.WriteColorLine("Контакти відсутні!", ConsoleColor.Red);
+                Console.ReadKey(true);
+            }
+            else
+            {
+                 ColorText.WriteColorLine("Список контактів:", ConsoleColor.Yellow);
+                     foreach (KeyValuePair<string,string> kv in contacts)
+                     {
+                         Console.WriteLine(kv.Key + " - " + kv.Value);
+                     }
+                Console.ReadKey(true);
+            }
+        }
+
+        public static void DeleteContact(Dictionary<string, string> contacts)
+        {
+            Console.Clear();
+            ColorText.WriteColorLine("Введіть ім'я контакту для видалення:", ConsoleColor.DarkGray);
+            string name = Console.ReadLine();
+            if (contacts.ContainsKey(name))
+            {
+                Console.WriteLine($"Ви впевнені, що хочете видалити контакт {name}? (y/n)");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    contacts.Remove(name);
+                    ColorText.WriteColorLine("\nКонтакт успішно видалено!", ConsoleColor.Green);
+                    Console.ReadKey(true);
+                }
+                else
+                {
+                    ColorText.WriteColorLine("\nВидалення скасовано.", ConsoleColor.Yellow);
+                    Console.ReadKey(true);
+                }
+            }
+            else
+            {
+                ColorText.WriteColorLine("Контакт з таким ім'ям не знайдено!", ConsoleColor.Red);
+                Console.ReadKey(true);
+            }
+        }
+
+    }
+}
---
+++ ContactBook/ContactBook/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotesAndPasswordGenerator;
+
+namespace ContactBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            bool Running = true;
+            bool invalidInput = false;
+
+            Dictionary<string, string> contacts = new Dictionary<string, string>();
+
+            while (Running)
+            {
+                Console.Clear();
+                if (invalidInput)
+                {
+                    ColorText.WriteColorLine("Не існуючий варіант, спробуйте ще раз!\n", ConsoleColor.Red);
+                    invalidInput = false;
+                }
+                ColorText.WriteColorLine("-< Книга контактів >-\n", ConsoleColor.Yellow);
+                Console.WriteLine("Виберіть дію:");
+
+                Console.WriteLine("1: Додати контакт");
+                Console.WriteLine("2: Вивести всі контакти");
+                Console.WriteLine("3: Видалити контакт");
+                Console.WriteLine("esc: Завжди назад");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        Contacts.AddContact(contacts);
+                        break;
+                    case ConsoleKey.D2:
+                        Contacts.ShowContacts(contacts);
+                        break;
+                    case ConsoleKey.D3:
+                        Contacts.DeleteContact(contacts);
+                        break;
+                    case ConsoleKey.Escape:
+                        Running = false;
+                        break;
+                    default:
+                        invalidInput = true;
+                        break;
+                }
+            }
+        }
+    }
+}
